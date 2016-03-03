f1b7259976a34b4100769b7b7b83aaec69a5ea5c
Martin31 <maarku75@gmail.com>
2016-03-02 23:34:46 -0800

    Basic Movement Added

--- vb2project2/vb2project2/Form1.vb
+++ vb2project2/vb2project2/Form1.vb
@@ -14,4 +14,22 @@ Public Class Form1
         'Private _speed As Double = 20.0
     End Class
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        Select Case e.KeyCode
+            Case Keys.Up
+                picPlayer.SetBounds(picPlayer.Left, picPlayer.Top - 5, 0, 0)
+            Case Keys.Down
+                picPlayer.SetBounds(picPlayer.Left, picPlayer.Top + 5, 0, 0)
+            Case Keys.Left
+                picPlayer.SetBounds(picPlayer.Left - 5, picPlayer.Top, 0, 0)
+            Case Keys.Right
+                picPlayer.SetBounds(picPlayer.Left + 5, picPlayer.Top, 0, 0)
+            Case Keys.Space
+
+        End Select
+    End Sub
 End Class
